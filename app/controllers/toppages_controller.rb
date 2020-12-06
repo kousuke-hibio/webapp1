074691fe83da120a772a7737c1c1d24fb2399bb5
kousuke.hibio <h.kusk0115@i.softbank.jp>
2020-12-06 15:16:32 +0900

    作業終了

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -4,5 +4,7 @@ class ToppagesController < ApplicationController
       @post = current_user.posts.build
       @posts = current_user.feed_posts.order(id: :desc).page(params[:page])
     end 
+    @posts = Post.limit(6).includes(:images).order('created_at DESC')
+    @ranks = User.find(Relationship.group(:follow_id).order('count(follow_id) desc').limit(3).pluck(:follow_id))
   end
 end
