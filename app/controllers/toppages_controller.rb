119c78a9cfa75b7216c02b17af439d3bf270a65f
kousuke.hibio <h.kusk0115@i.softbank.jp>
2020-12-11 22:56:53 +0900

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -6,5 +6,6 @@ class ToppagesController < ApplicationController
     end 
     @posts = Post.limit(6).includes(:images).order('created_at DESC')
     @ranks = User.find(Relationship.group(:follow_id).order('count(follow_id) desc').limit(3).pluck(:follow_id))
+    @rank = User.find(Relationship.group(:follow_id).order('count(follow_id) desc').limit(5).pluck(:follow_id))
   end
 end
