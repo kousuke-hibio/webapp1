ac6e8dfffde23df9c750d6be321bf0053f96df47
kousuke.hibio <h.kusk0115@i.softbank.jp>
2020-12-21 18:51:03 +0900

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,49 @@
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '3.14.1'
+
+# Capistranoのログの表示に利用する
+set :application, 'webapp1'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:kousuke-hibio/webapp1.git'
+
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, 2.5.1 #<このアプリで使用しているrubyのバージョン>
+
+# credentials.yml.encではmasterkeyにする
+set :linked_files, %w{config/master.key}
+
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/hibio0115.pem'] 
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:stop'
+    invoke 'unicorn:start'
+  end
+  desc 'upload master.key'
+ task :upload do
+   on roles(:app) do |host|
+     if test "[ ! -d #{shared_path}/config ]"
+       execute "mkdir -p #{shared_path}/config"
+     end
+     upload!('config/master.key', "#{shared_path}/config/master.key")
+   end
+ end
+ before :starting, 'deploy:upload'
+ after :finishing, 'deploy:cleanup'
+end
