88ea0b990b709e53ad0020bb095a64ec8e1e912d
kousuke.hibio <h.kusk0115@i.softbank.jp>
2020-12-22 12:30:47 +0900

    変更

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,9 +3,10 @@ class ToppagesController < ApplicationController
     if logged_in?
       @post = current_user.posts.build
       @posts = current_user.feed_posts.order(id: :desc).page(params[:page])
+      @rank = User.find(Relationship.group(:follow_id).order('count(follow_id) desc').limit(5).pluck(:follow_id))
+    else  
+      @posts = Post.limit(6).includes(:images).order('created_at DESC')
+      @ranks = User.find(Relationship.group(:follow_id).order('count(follow_id) desc').limit(3).pluck(:follow_id))
     end 
-    @posts = Post.limit(6).includes(:images).order('created_at DESC')
-    @ranks = User.find(Relationship.group(:follow_id).order('count(follow_id) desc').limit(3).pluck(:follow_id))
-    @rank = User.find(Relationship.group(:follow_id).order('count(follow_id) desc').limit(5).pluck(:follow_id))
   end
 end
